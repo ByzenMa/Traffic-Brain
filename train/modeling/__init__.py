f370a2eccd1deaa073518c99c133ec722e275605
ByzenMa <Baizhang.Ma@gmail.com>
2026-03-23 13:11:54 +0800

    Add configurable baseline enhancement modules

--- train/modeling/__init__.py
+++ train/modeling/__init__.py
@@ -5,10 +5,30 @@
 """
 
 from .baseline import Baseline
+from .baseline_modular import ConfigurableBaseline
 
 
 def build_model(cfg, num_classes):
     # if cfg.MODEL.NAME == 'resnet50':
     #     model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT)
-    model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE)
+    use_modular_model = any([
+        cfg.MODEL.MODULES.USE_MSF,
+        cfg.MODEL.MODULES.USE_RSCAMA,
+        cfg.MODEL.MODULES.USE_TFF,
+        cfg.MODEL.MODULES.USE_SFF,
+    ])
+
+    if use_modular_model:
+        model = ConfigurableBaseline(
+            num_classes,
+            cfg.MODEL.LAST_STRIDE,
+            cfg.MODEL.PRETRAIN_PATH,
+            cfg.MODEL.NECK,
+            cfg.TEST.NECK_FEAT,
+            cfg.MODEL.NAME,
+            cfg.MODEL.PRETRAIN_CHOICE,
+            cfg,
+        )
+    else:
+        model = Baseline(num_classes, cfg.MODEL.LAST_STRIDE, cfg.MODEL.PRETRAIN_PATH, cfg.MODEL.NECK, cfg.TEST.NECK_FEAT, cfg.MODEL.NAME, cfg.MODEL.PRETRAIN_CHOICE)
     return model
